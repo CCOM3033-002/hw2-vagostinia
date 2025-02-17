0b81d5d70b70769f490f9fae6ac17e5629175afb
vagostinia <victoria.agostini@upr.edu>
2025-02-17 22:54:12 +0000

    C2

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main(){
-  // escriba su código aquí 
+  
       // Mensaje de bienvenida
       cout << "Este programa suma dos fracciones." << endl;
 
@@ -12,22 +12,22 @@ int main(){
       int num1, den1, num2, den2;
   
       // Pedir la primera fracción
-      cout << "Ingrese el numerador de la 1ra fracción: ";
+      cout << "Entre el numerador de la 1ra fracción: ";
       cin >> num1;
-      cout << "Ingrese el denominador de la 1ra fracción: ";
+      cout << "Entre el denominador de la 1ra fracción: ";
       cin >> den1;
   
       // Pedir la segunda fracción
-      cout << "Ingrese el numerador de la 2da fracción: ";
+      cout << "Entre el numerador de la 2da fracción: ";
       cin >> num2;
-      cout << "Ingrese el denominador de la 2da fracción: ";
+      cout << "Entre el denominador de la 2da fracción: ";
       cin >> den2;
   
       // Calcular la suma de fracciones
       int numeradorResultado = (num1 * den2) + (num2 * den1);
       int denominadorResultado = den1 * den2;
   
-      // Mostrar el resultado en formato fracción
+      // Mostrar el resultado 
       cout << "La suma es " << numeradorResultado << "/" << denominadorResultado << "." << endl;
   return 0;
 }
